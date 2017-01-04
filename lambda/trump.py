fa482d09581c808e0d690334f7154ed3b3abe41a
Dave Voutila <voutilad@gmail.com>
2017-01-04 16:06:05 -0500

    skeleton

---
+++ lambda/trump.py
@@ -0,0 +1,13 @@
+from __future__ import print_function
+import requests
+
+
+def lambda_handler(event, context):
+    """
+    Main entry-point for the Lambda function.
+    """
+    print('Hello world!')
+    return
+
+if __name__ == '__main__':
+    lambda_handler(None, None)
